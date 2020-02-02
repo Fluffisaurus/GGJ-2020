4bcac5ef4b40bcaa5ed95e4a50856a30163520bf
christo <christo@sfu.ca>
2020-02-01 23:23:08 -0800

    Partial Cat animation in process, finished rat animation

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -6,13 +6,29 @@ public class Moving : Entity
 {
     public Vector2Int direction;
     public bool active;
+    public Sprite[] cat_imgs;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    if (active) {
+        if(direction.x == 1 && direction.y == 0) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = cat_imgs[1];
+        }
+        else if(direction.x == -1 && direction.y == 0) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = cat_imgs[2];
+        }
+        else if(direction.x == 0 && direction.y == 1) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = cat_imgs[3];
+        }  
+        else if(direction.x == 0 && direction.y == -1) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = cat_imgs[0];
+        }
+    
     internal void SwitchDirection()
     {
+        //direction change
         direction = direction * -1;
     }
     internal override Vector2Int Simulate()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,30 @@ public class Player : Entity
 {
    public Vector2Int direction;
 
+   public Sprite[] rat_imgs;
+
     private List<Stationary> stationaries;
     internal void SetupSimulate(List<Stationary> stationaries, int x, int y)
     {
         this.stationaries  = stationaries;
         this.direction =  new Vector2Int(x,y);
+        //depending on what direction is, update image accordingly
+
+        // if(x == 0 && y == 0) {
+        //     rat.GetComponent<SpriteRenderer>().sprite = rat_imgs[0];
+        // }
+        if(x == 1 && y == 0) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = rat_imgs[1];
+        }
+        else if(x == -1 && y == 0) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = rat_imgs[2];
+        }
+        else if(x == 0 && y == 1) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = rat_imgs[0];
+        }
+        else if(x == 0 && y == -1) {
+            gameObject.GetComponent<SpriteRenderer>().sprite = rat_imgs[3];
+        }
     }
     internal override Vector2Int Simulate()
     {

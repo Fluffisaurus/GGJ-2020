a43410b7a25749ce4205db6cfd8685b8d773166d
christo <christo@sfu.ca>
2020-02-02 01:22:50 -0800

    Cat prefab done?

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -7,6 +7,7 @@ public class Moving : Entity
     public Vector2Int direction;
     public bool active;
     public Sprite[] Cat_imgs;
+    public Sprite[] CatImgsSleep;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +15,33 @@ public class Moving : Entity
     }
 
     public void SwitchImages() {
+        if(active){
             if(direction.x == 0 && direction.y == 1) {
                 gameObject.GetComponent<SpriteRenderer>().sprite = Cat_imgs[0];
             }
-            // if(direction.x == 0 && direction.y == 0) {
-            //     gameObject.GetComponent<SpriteRenderer>().sprite = Cat_imgs[1];
-            // }
-            // else if(direction.x == -1 && direction.y == 0) {
-            //     gameObject.GetComponent<SpriteRenderer>().sprite = Cat_imgs[2];
-            // }
             else if(direction.x == 0 && direction.y == -1) {
                 gameObject.GetComponent<SpriteRenderer>().sprite = Cat_imgs[3];
             }  
+            if(direction.x == -1 && direction.y == 0) {
+                gameObject.GetComponent<SpriteRenderer>().sprite = Cat_imgs[2];
+            }
+            else if(direction.x == 1 && direction.y == 0) {
+                gameObject.GetComponent<SpriteRenderer>().sprite = Cat_imgs[1];
+            }
+        } else {
+            if(direction.x == 0 && direction.y == 1) {
+                gameObject.GetComponent<SpriteRenderer>().sprite = CatImgsSleep[0];
+            }
+            else if(direction.x == 0 && direction.y == -1) {
+                gameObject.GetComponent<SpriteRenderer>().sprite = CatImgsSleep[3];
+            }  
+            if(direction.x == -1 && direction.y == 0) {
+                gameObject.GetComponent<SpriteRenderer>().sprite = CatImgsSleep[2];
+            }
+            else if(direction.x == 1 && direction.y == 0) {
+                gameObject.GetComponent<SpriteRenderer>().sprite = CatImgsSleep[1];
+            }
+        }
     }
     
     internal void SwitchDirection()
@@ -37,19 +53,24 @@ public class Moving : Entity
             direction = direction * -1; 
             SwitchImages();
         }
+
     }
     internal override Vector2Int Simulate()
     {
         int size = GridObject.i.size;
         Vector2Int newPos =  pos + direction;
-        if(newPos.x < 0 || newPos.x >= size || newPos.y < 0 || newPos.y >= size)
-        {
-            SwitchDirection();
-            return Simulate();
-        }
-        else
-        {
-            return newPos;
+        if(active){
+            if(newPos.x < 0 || newPos.x >= size || newPos.y < 0 || newPos.y >= size)
+            {
+                SwitchDirection();
+                return Simulate();
+            }
+            else
+            {
+                return newPos;
+            }
+        }else {
+            return pos;
         }
     }
 }
